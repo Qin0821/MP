9df0b91942d79f94e4c4cfb8e931d83ad0aa8550
Qin <liuqin0821@gmail.com>
2020-12-28 00:04:58 +0800

    完成礼遇快闪开发版

--- miniprogram/pages/invitation/luolai/luolai.js
+++ miniprogram/pages/invitation/luolai/luolai.js
@@ -6,7 +6,8 @@ Page({
    */
   data: {
     logoArray: [false, false, false, false, false],
-    isAllComplete: false
+    isAllComplete: false,
+    lightName: false,
   },
 
   doScan: function() {
@@ -17,23 +18,18 @@ Page({
         var logoArr = this.data.logoArray
         switch(res.result) {
           case '1': 
-            console.log(1)
             logoArr[0] = true
             break;
           case '2': 
-            console.log(2)
             logoArr[1] = true
             break;
           case '3': 
-            console.log(3)
             logoArr[2] = true
             break;
           case '4': 
-            console.log(4)
             logoArr[3] = true
             break;
           case '5': 
-            console.log(5)
             logoArr[4] = true
             break;
           default:
@@ -41,17 +37,73 @@ Page({
         }
         this.setData({
             logoArray: logoArr,
-            isAllComplete: logoArr.indexOf(false) == -1
         })
+        
+        wx.setStorage({
+          key: "logoArray",
+          data: JSON.stringify(logoArr)
+        })
+
+        if (logoArr.indexOf(false) == -1) {
+          this.doComplete()
+        }
       }
     })
+  },
+
+  doComplete: function() {
+    var that = this
+    var duration = 3000
+    var ratate = 1800
+    this.animate('.name', [
+      { opacity: 1.0, rotate: 0, scaleX: 1, ease: 'ease-in'},
+      { opacity: 0, rotate: ratate, scaleX: 0.5, ease: 'ease-in'},
+      ], duration, function () {
+          that.setData({
+            lightName: true
+          })
+        this.animate('.name', [
+          { opacity: 0, rotate: ratate, scaleX: 0.5, ease: 'ease-out'},
+          { opacity: 1.0, rotate: ratate * 2, scaleX: 1, ease: 'ease-out'},
+          ], duration, function () {
+            that.setData({
+              isAllComplete: true
+            })
+            this.animate('.gift', [
+              { opacity: 0, scale3d: [0.5, 0.5, 0.5], ease: 'ease-out'},
+              { opacity: 1.0, scale3d: [1, 1, 1], ease: 'ease-out'},
+              ], 1000, function () {
+            }.bind(this))
+        }.bind(this))
+    }.bind(this))
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    wx.setNavigationBarTitle({
+      title: '礼遇快闪'
+    })
 
+    wx.hideShareMenu({
+      success: (res) => {
+        console.log('hideShareMenu')
+      },
+    })
+
+    var that = this
+    wx.getStorage({
+      key: 'logoArray',
+      success(res) {
+        console.log(res)
+        var logoArr = JSON.parse(res.data)
+        that.setData({
+          logoArray: logoArr,
+          isAllComplete: logoArr.indexOf(false) == -1
+        })
+      }
+    })
   },
 
   /**
